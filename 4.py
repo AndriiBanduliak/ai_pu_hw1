950a7d7502da5c4ee4290bd85eee74e62a4635ae
Banduliak Andrii <109424920+AndriiBanduliak@users.noreply.github.com>
2022-12-12 21:44:15 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,13 +1,11 @@
-X = int(input('Enter the x = '))
-Y = int(input('Enter the y = '))
-
-if X > 0 and Y > 0:
-    print("The point in the 1th quarter ")
-elif X < 0 and Y > 0:
-    print('The point in the 2nd quarter')
-elif X < 0 and Y < 0:
-    print('The point in the 3rd quarter')
-elif X > 0 and Y < 0:
-    print('The point in the 4th quarter')
+num_quarter = float(input('Enter quarter number: '))
+if num_quarter == 1:
+    print('Coordinate range: X > 0 and Y > 0')
+elif num_quarter == 2:
+    print('Coordinate range: X < 0 and Y > 0')
+elif num_quarter == 3:
+    print('Coordinate range: X < 0 and Y < 0')
+elif num_quarter == 4:
+    print('Coordinate range: X > 0 and Y < 0')
 else:
-    print("It's the center of the coordinates")
+    print('There is no quarter')
